ee1e55088f2759cae664f3ed69749c1efc73ae2f
wuzzapcom <wuzzapcom@gmail.com>
2017-11-19 12:54:40 +0300

    Added erasing log file before program start

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+void clearLogsFile();
+
 int main(int argc, char *argv[])
 {
+    clearLogsFile();
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
+}
+
+void clearLogsFile()
+{
+    QFile csv("logs.csv");
+    csv.open(QFile::WriteOnly);
+    csv.close();
 }
